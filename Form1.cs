790ac79f4dab35037c036f01d154e0b48eff2fd1
anteskov <anteskovacevic@gmail.com>
2014-08-29 10:20:38 +0200

    change nbr 1
    change

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace testgit
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("this is test change");
+        }
     }
 }
